e7dc57d14031b3e717b6dd8e5f414dbc7a5d47c4
tliawi <jhn.frfld@gmail.com>
2021-02-15 21:30:03 -0500

    Update BMHC.js
    ids, refs [id], and namerefs [name]. Stored affiliations are ids,
    stored namerefs in notes are refs, all this so an assembly name can be changed without side effects.

--- BMHC.js
+++ BMHC.js
@@ -6,6 +6,40 @@ Copyright 2021 John R C Fairfield, see MIT License
 
 function BMHCobj(){
     
+    
+    const brethren  = 1;
+    const mennonite = 2;
+    // 0 neither, 3 both
+    // name:val pairs, in names no double quotes, no ampersand. Blanks, Apostrophe are OK
+    
+    // unfinished 
+    // means to add labels (never remove them)
+    // save db to storage somewhere
+    var db = {
+        idSource:123, //source of unique assembly ids.
+        verbs:{"set-locale":1, "set-weight":1, "set-affiliation":1, "photo":1, "set-label":1, "see-note":1,}, //see checkObject()
+        labels:{"paid minister","flag in sanctuary",},
+        assemblies:{}, //a dictionary of assemblyData, the keys being assembly names
+    }
+    
+    var idToName = {}; //a dictionary of id:assemblyName pairs.
+    
+    //id's are references, "[d+]"
+    //the reason for references, and idToName, is so one can change the name of an assembly
+    //and all the stored object and note references do NOT have to change.
+    ---todo next: replace name refs with id refs in notes. Do assembly name change.
+    function getNextId(){
+        return "["+(db.idSource++)+"]";
+    }
+            
+    function assemblyData(mb){
+        this.id = getNextId();
+        this.mb = mb;
+        this.events = []; //an array of events comprising the history of an assembly
+        this.states = []; //an array of states parallel to events. Display accellerator.
+    }
+    
+    
     function sortObj(obj) {
       return Object.keys(obj).sort(new Intl.Collator('en').compare).reduce(function (result, key) {
         result[key] = obj[key];
@@ -13,93 +47,162 @@ function BMHCobj(){
       }, {});
     }
     
-    const brethren  = 1;
-    const mennonite = 2;
-    // 0 neither, 3 both
-    // name:val pairs, in names no double quotes, no ampersand. Blanks, Apostrophe are OK
-    
-    var assemblies = {}; //a dictionary of assemblyData, the keys being assembly names
-    
-    function event(MIdate,verb,object,note){ //all strings
-        this.date = date;
-        this.verb = verb;
-        this.object = object;
-        this.note = note;
-    }
-    
-    function assemblyData(mb){
-        this.mb = mb;
-        this.events = []; //an array of events comprising the history of an assembly
-        this.states = []; //an array of states parallel to events. Display accellerator.
+    function buildIdToName(){
+        Object.entries(db.assemblies).forEach(([name, assemblyData]) => idToName[assemblyData.id] = name);
     }
     
     function init(){
-        var mockupAssemblies = {
-            "2nd District of Virginia":{mb:1,events:[],states:[]},
-            "Brethren Woods Camp and Retreat Center":{mb:1,events:[],states:[]},
-            "Bridgewater":{mb:1,events:[],states:[]},
-            "Church of the Brethren":{mb:1,events:[],states:[]},
-            "Cooks Creek":{mb:1,events:[],states:[]},
-            "Dayton":{mb:1,events:[],states:[]},
-            "Garber's":{mb:1,events:[],states:[]},
-            "German Baptist Brethren":{mb:1,events:[],states:[]},
-            "Harrisonburg First":{mb:1,events:[],states:[]},
-            "Meeting at Solomon Garber home":{mb:1,events:[],states:[]},
-            "Park View":{mb:2,events:[],states:[]},
-            "Community (Harrisonburg)":{mb:2,events:[],states:[]},
-            "Dayton Mennonite":{mb:2,events:[],states:[]},
-            "Pike":{mb:2,events:[],states:[]}, 
-            "Bank":{mb:2,events:[],states:[]},
-            "Weaver's":{mb:2,events:[],states:[]},
-            "Harrisonburg":{mb:2,events:[],states:[]},
-            "Pleasant View":{mb:2,events:[],states:[]},
-            "Virginia Conference":{mb:2,events:[],states:[]},
-            "MCUSA":{mb:2,events:[],states:[]},
-            "Anne Arbor":{mb:3,events:[],states:[]},
-            "the National Cathedral":{mb:0,events:[],states:[]},
+        const mockupAssemblies = {
+            "2nd District of Virginia":{id:10, mb:1,events:[],states:[]},
+            "Brethren Woods Camp and Retreat Center":{id:2, mb:1,events:[],states:[]},
+            "Bridgewater":{id:4, mb:1,events:[],states:[]},
+            "Church of the Brethren":{id:3, mb:1,events:[],states:[]},
+            "Cooks Creek":{id:8, mb:1,events:[],states:[]},
+            "Dayton":{id:7, mb:1,events:[],states:[]},
+            "Garber's":{id:6, mb:1,events:[],states:[]},
+            "German Baptist Brethren":{id:1, mb:1,events:[],states:[]},
+            "Harrisonburg First":{id:20, mb:1,events:[],states:[]},
+            "Meeting at Solomon Garber home":{id:18, mb:1,events:[],states:[]},
+            "Park View":{id:19, mb:2,events:[],states:[]},
+            "Community (Harrisonburg)":{id:17, mb:2,events:[],states:[]},
+            "Dayton Mennonite":{id:16, mb:2,events:[],states:[]},
+            "Pike":{id:15, mb:2,events:[],states:[]}, 
+            "Bank":{id:14, mb:2,events:[],states:[]},
+            "Weaver's":{id:12, mb:2,events:[],states:[]},
+            "Harrisonburg":{id:13, mb:2,events:[],states:[]},
+            "Pleasant View":{id:25, mb:2,events:[],states:[]},
+            "Virginia Conference":{id:27, mb:2,events:[],states:[]},
+            "MCUSA":{id:5, mb:2,events:[],states:[]},
+            "Anne Arbor":{id:21, mb:3,events:[],states:[]},
+            "the National Cathedral":{id:22, mb:0,events:[],states:[]},
         };
         
-        //philosopy: maintain assemblies in sorted key order, is read more often than written.
-        assemblies = sortObj(mockupAssemblies); 
+        const mockupBridgewaterEvents = [
+            new event("1878","set-locale","38.3771809888396,-79.03095281203701",""),
+            new event("1878","set-affiliation","[8]",""),
+            new event("1878/06/22","photo","https://photos.app.goo.gl/XCJtqEQEyVsfogGEA",""),
+            new event("1907","set-affiliation","[10]","[4] becomes independent from [8]"),
+            new event("1907","set-weight","201"),
+            new event("1915","set-label","paid minister"),
+        ];
         
+        //philosopy: maintain assemblies in sorted key order, is read more often than written.
+        db.assemblies = sortObj(mockupAssemblies);
+        
+        mockupBridgewaterEvents.forEach(event => addEvent(event)
+        buildIdToName();
     }
         
     function getAllAssemblies(){
-        return Object.keys(assemblies);
+        return Object.keys(db.assemblies);
     }
     
     function getMennoniteAssemblies(){
-        return getAllAssemblies().filter(name => assemblies[name].mb & mennonite);
+        return getAllAssemblies().filter(name => db.assemblies[name].mb & mennonite);
     }
     
     function getBrethrenAssemblies(){
-        return getAllAssemblies().filter(name => assemblies[name].mb & brethren);
+        return getAllAssemblies().filter(name => db.assemblies[name].mb & brethren);
     }
     
     function getJointAssemblies(){
-        return getAllAssemblies().filter(name => assemblies[name].mb); //i.e. mb != 0
+        return getAllAssemblies().filter(name => db.assemblies[name].mb); //i.e. mb != 0
     }
     
     function getNeitherAssemblies(){
-        return getAllAssemblies().filter(name => assemblies[name].mb == 0);
+        return getAllAssemblies().filter(name => db.assemblies[name].mb == 0);
     }
     
     function assemblyExists(name){
-        return (name in assemblies);
+        return (name in db.assemblies);
     }
     
     function addAssembly(name,mb){ //mb 00:neither 10:Mennonnite 01:Brethren 11:both
         if (assemblyExists(name))return false;
-        assemblies[name] = new assemblyData(mb);
-        assemblies = sortObj(assemblies);
+        db.assemblies[name] = new assemblyData(mb);
+        db.assemblies = sortObj(db.assemblies);
         return true;
     }
     
     function deleteAssembly(name){
         //tosses assembly and its assemblyData, including all events therein.
-        delete assemblies[name]; //doesn't throw anything if it doesn't exist.
+        delete db.assemblies[name]; //doesn't throw anything if it doesn't exist.
         
         //.........still have to go through and delete all events having this name as object ?? Or just leave them as historical anomalies?
+    }
+    
+    //returns "ok" if it succeeds, else an error statement
+    function addEvent(subjectAssembly,date,verb,object,note){ //all these are strings as given by user
+        
+        function event(MIdate,verb,object,note){
+            this.date = date;
+            this.verb = verb;
+            this.object = object;
+            this.note = note;
+        }
+        
+        function placeEvent(subjectAssembly,date,verb,object,note){
+            var evs = db.assemblies[subjectAssembly].events;
+            evs[evs.length] = new event(date,verb,object,note)
+        }
+        
+        function compareDate(e1,e2){
+            return (e1.date < e2.date)?-1:((e1.date < e2.date)?1:0);
+        }
+        
+        subjectAssembly = subjectAssembly.trim;
+        date = date.trim;
+        verb = verb.trim;
+        object = object.trim;
+        note = note.trim;
+        if (!assemblyExists(subjectAssembly)) return "subject assembly doesn't exist";
+        else if (!checkDate(date)) return "bad date";
+        else {
+            const critique = okVerbObject(verb,object);
+            if (critique != "ok") return critique;
+            if (verb == set-affiliation) object = db.assemblies[subjectAssembly].id; // !!!
+            //unfinished: replace [references] in notes---------
+            const evs = db.assemblies[subjectAssembly].events;
+            evs.push(new event(date,verb,object,note));
+            evs.sort((e1,e2)=>compareDate(e1,e2));
+            return "ok";
+        }
+    }
+    
+    function checkVerb(verb){
+        return (verb in db.verbs);
+    }
+    
+    function okVerbObject(verb, object){
+        if (checkVerb(verb)){
+            switch (verb){
+                case "set-locale":
+                    if (object.length > 0) return "ok"; //------unfinished checkGPS(object as gpscoord)
+                    else return "bad GPS coordinates";
+                case "set-weight":
+                    if (object.length && allDigits(0,object.length)) return "ok";
+                    else return "bad weight amount";
+                case "set-affiliation":
+                    if (assemblyExists(object)) return "ok";
+                    else return "unrecognized assembly name";
+                case "photo": 
+                    if (object.length > 0) return "ok"; //---------------unfinished checkURL??? 
+                    else return "bad URL";
+                case "set-label":
+                    if (object in db.labels) return "ok";
+                    else return "unrecognized label";
+                case "see-note":
+                    if (object.length > 0) return "ok";
+                    else return "nothing in note";
+                default: "verb not recognized";
+            }
+        } else return "unrecognized verb";
+    }
+    
+    function allDigits(start,len){
+        var snippet = candidate.substr(start,len);
+        if ( snippet.length != len) return false;
+        return /^\d+$/.test(snippet);
     }
     
     //A date 
@@ -110,12 +213,6 @@ function BMHCobj(){
     function checkDate(candidate){
         
         function remainderBlank(i){ return candidate.substr(i).trim().length == 0;}
-        
-        function allDigits(start,len){
-            var snippet = candidate.substr(start,len);
-            if ( snippet.length != len) return false;
-            return /^\d+$/.test(snippet);
-        }
         
         if (candidate.length != 10) return false;
         
@@ -132,8 +229,29 @@ function BMHCobj(){
         return true; //yyyy/mm/dd
     }
     
+    //returns an array of eventToStrings corresponding to the events of an assembly
     function getEventStrings(assembly){ 
-        return ["   1 1878/04/27 set-locale 38.3771809888396, -79.03095281203701"];
+        
+        function renderIDs(str){ //replace each [id] reference with [name] reference
+            const refs = str.match(/\[\d+\]/g) ; //returns an array of refs "[d+]" from the strings
+            refs.forEach(ref=>str.replace(ref,idToName[ref]);
+        }
+        
+        //flesh out str with trailing blanks to be (at least) len chars long.
+        function buff(str,len){ 
+            if (len <= str.length) return str;
+            else return str + " ".repeat(len-str.length);
+        }
+        
+        function eventToString(ev){
+            return buff(ev.date,10) + " " + ev.verb + " " + ((ev.verb == "set-affiliation")?idToName[ev.obj]:ev.obj) + " " + renderIDs(ev.note);
+        }
+        
+        if (assemblyExists(assembly)) {
+            const evs = db.assemblies[assembly].events;
+            return db.assemblies[assembly].events.map(ev => eventToString(ev));
+        }
+        else return [];
     }
     
     init();
@@ -147,5 +265,6 @@ function BMHCobj(){
             deleteAssembly:deleteAssembly,
             getEventStrings:getEventStrings,
             checkDate:checkDate,
+            idToName:idToName,
            };
 }
